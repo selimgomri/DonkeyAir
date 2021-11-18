7f4fad70685e3239d3a8cf74cebbe4335676fe6e
Selim GOMRI <selimgomri@gmail.comssh git github.comssh git@github.comcdssh git@github.comss-keygen -t rsagit config --global user.email selimgomri@gmail.com>
2021-11-18 19:32:42 +0100

    booking works only if you put depart arrivée and date

---
+++ booking.php
@@ -0,0 +1,57 @@
+<?php
+session_start();
+@require_once "connectDB.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Flight results</title>
+</head>
+
+<body>
+    <?php
+    if ((in_array('', $_POST))) {
+        header("Location: homepage.html");
+    }
+    else {
+        $departureAirport=$_POST['departureAirport'];
+        $arrivalAirport=$_POST['arrivalAirport'];
+        $departureTime=$_POST['departureTime'];
+        
+        $query="SELECT flightNumber, departureAirport, arrivalAirport, departureTime, arrivalTime, price FROM flight WHERE 
+        departureAirport= :departureAirport
+        AND arrivalAirport= :arrivalAirport 
+        AND departureTime>= :departureTime
+        ORDER BY price ASC";
+
+        //preparation PDO
+        $statement = $pdo->prepare($query);
+        $statement->bindValue(':departureAirport', $departureAirport, \PDO::PARAM_STR);
+        $statement->bindValue(':arrivalAirport', $arrivalAirport, \PDO::PARAM_STR);
+        $statement->bindValue(':departureTime', $departureTime, \PDO::PARAM_STR);
+        
+        
+        $statement->execute();
+        //end of preparation
+
+        $flights = $statement->fetchAll();
+
+        foreach($flights as $flights) {
+            echo $flights['flightNumber'] . " | " . 
+                $flights['departureAirport'] . " | " .
+                $flights['arrivalAirport'] . " | " . 
+                $flights['departureTime'] . " | " . 
+                $flights['arrivalTime'] . " | " . 
+                $flights['price'] . " | " . 
+            "<br>";
+        }
+
+    }
+    ?>
+    
+</body>
+</html>
