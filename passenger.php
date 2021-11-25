41fa2be4f88bf044f0d341d617be08420b470f5a
Selim GOMRI <selimgomri@gmail.comssh git github.comssh git@github.comcdssh git@github.comss-keygen -t rsagit config --global user.email selimgomri@gmail.com>
2021-11-25 18:56:29 +0100

    made passenger form with a for loop

--- passenger.php
+++ passenger.php
@@ -1,52 +1,51 @@
+<!DOCTYPE html>
+<html lang="fr">
 
+<head>
+    <meta charset="UTF-8">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description"
+        content="Pour reserver votre vol au meilleur prix et vers les plus grandes destinations, Donkey Airlines vous propose le meilleur confort et la meilleure expérience à bord. ">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <link rel="stylesheet" href="fancy.css">
+    <title>Informations passagers</title>
+</head>
 
-  <?php
-  $bdd = new PDO('mysql:host=127.0.0.1:3306;dbname=donkeyAirDB','root',"");
-  
-  $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  if(isset($_POST['confirmer'])){
-  
-  }
- 
- 
-  $prenom = $_POST['firstname'];
-  $nom = $_POST['lastname'];
-  $email = $_POST['email'];
-  $dateDeNaissance = $_POST['birthDate'];
-  
-  if(!empty($prenom) && !empty($nom) &&  !empty($email) &&  !empty($dateDeNaissance))
-  
+<body>
+    <?php @require_once "header.php"; ?>
+    <h1 class="display-5">Informations passagers</h1>
+    <form class="reservationContainerLogin">
+        <?php
+    for ($i=1; $i<6; $i++) { ?>
+        <form class="reservationContainerLogin">
 
-         
-          $requet = $bdd->prepare('INSERT INTO user(firstname,lastname,email, birthDate)VALUE(:firstname, :lastname, :email, :birthDate)');
-  
-         
-          $requet->bindvalue(':firstname',$prenom);
-          $requet->bindvalue(':lastname',$nom);
-          $requet->bindvalue(':email',$email);
-          $requet->bindvalue(':birthDate',$dateDeNaissance);
-  
-          $result=$requet->execute();
-          var_dump($result);
-          
-          if (!$result) {
-             
-             echo " Un probleme ets survenu, L'enregistrement n'a pas été effectué! ";
-             
-            
-         }
-          
-         
-         
-         
+            <h2>Passager <?php echo $i ?></h2>
+            <label for="firstname"></label>
+            <input type="text" id="firstname" autocomplete="on" name="firstname" placeholder="Prénom">
 
-        
-        
-       
-         
-     
-  
-  ?>
+            <label for="lastname"></label>
+            <input type="text" id="lastname" autocomplete="on" name="lastname" placeholder="Nom">
 
+            <label for="birthdate"></label>
+            <input type="date" id="birthdate" autocomplete="on" name="birthdate" placeholder="birthdate">
 
-    
+            <label for="email"></label>
+            <input type="email" id="email" autocomplete="on" name="email" placeholder="Adresse email">
+
+            <label for="phone"></label>
+            <input type="tel" id="phone" autocomplete="on" name="phone" placeholder="Téléphone">
+
+        </form>
+        <?php
+    }
+    ?>
+        <input class="validatebtn" type="submit" value="Suivant">
+    </form>
+
+    <?php @require_once "footer.html" ?>
+</body>
+
+</html>
