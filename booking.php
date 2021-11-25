e3c76613dd160a6f650d67795fb3f1d458c27c4a
simoncm <mollardsimon11@gmail.com>
2021-11-25 09:39:43 +0100

    putting header back in loginpage.php for a different menu heading back to index.php

--- booking.php
+++ booking.php
@@ -9,10 +9,11 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Pour reserver votre vol au meilleur prix et vers les plus grandes destinations, Donkey Airlines vous propose le meilleur confort et la meilleure expérience à bord. ">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <link rel="stylesheet" href="fancy.css">
-    <title>Flight results</title>
+    <title>Reservez votre vol aller-retour au meilleur prix avec Donkey Airlines</title>
 </head>
 
 <body>
@@ -50,7 +51,7 @@ session_start();
 
                         $flights = $statement->fetchAll(); ?>
                         <div class="display-8">
-                            <?php echo "VOLS ALLER"; ?>
+                            <h2> <?php echo "VOLS ALLER"; ?> </h2>
                         </div>
                         <?php
                         if (empty($flights)) {
@@ -73,9 +74,37 @@ session_start();
                                 </button>
                             </div> 
                             <div class="packageResults-container">
-                                <div class="packageResults">Pack1</div>
-                                <div class="packageResults">Pack2</div>
-                                <div class="packageResults">Pack3</div>
+                                <div class="packageResults">
+                                    <div class="packageResultTitle1">
+                                        <h4>SAVER</h4>
+                                    </div>
+                                    <div class="packageResultContent">
+                                        <li>🧳 Aucun bagage inclu</li><br>
+                                        <li>💺 Siège attribué à l'enregistrement</li><br>
+                                        <li>✔️ 5000 Miles</li><br>
+                                        <button id="packageButtonChoice">
+                                            <?php echo $values['price'] . " € "; ?>
+                                        </button>
+                                    </div>
+                                </div>
+                                <div class="packageResults">
+                                    <div class="packageResultTitle2">
+                                        <h4>FLEX</h4>
+                                    </div>
+                                    <div class="packageResultContent">
+                                        <li>🧳 1 x 23kg</li><br>
+                                        <li>💺 Choix du siège</li><br>
+                                        <li>✔️ 20000 Miles</li><br>
+                                        <button id="packageButtonChoice">
+                                            <?php echo $values['price'] . " € "; ?>
+                                        </button>
+                                    </div>
+                                </div>
+                                <div class="packageResults">
+                                    <div class="packageResultTitle3">
+                                        <h4>PREMIUM</h4>
+                                    </div>
+                                </div>
                             </div>
                         </br>
                         <?php
--- index.php
+++ index.php
@@ -25,9 +25,10 @@ $arrivalAirports=array_unique($arrivalAirports);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Pour reserver votre vol au meilleur prix et vers les plus grandes destinations, Donkey Airlines vous propose le meilleur confort et la meilleure expérience à bord. ">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <link rel="stylesheet" href="fancy.css">
-    <title>Donkey Airlines</title>
+    <title>Reservez votre vol aller-retour au meilleur prix avec Donkey Airlines</title>
 </head>
 
 <body>
