54671d13ebc41eff58d2806ecd5b839e022788a9
Selim GOMRI <selimgomri@gmail.comssh git github.comssh git@github.comcdssh git@github.comss-keygen -t rsagit config --global user.email selimgomri@gmail.com>
2021-11-27 12:35:16 +0100

    summary page displays oneWay and return flights, passenger owner and total price.  UX is shit.

--- summary.php
+++ summary.php
@@ -12,7 +12,6 @@
         content="Pour reserver votre vol au meilleur prix et vers les plus grandes destinations, Donkey Airlines vous propose le meilleur confort et la meilleure expérience à bord. ">
     <link rel="stylesheet" href="fancy.css">
     <title>Récapitulatif de votre vol</title>
-
 </head>
 
 <body>
@@ -31,43 +30,170 @@
         <h1 class="display-7">Récapitulatif de votre réservation</h1>
         <div class="displayBoxShadow">
             <div class="bookingResultBoxes">
-                <div class="flexResults">
-                    <?php
-                    $oneWayFlight=$_SESSION['oneWayFlight'];
-                    $returnWayFlight=$_SESSION['returnFlight'];
-                    $passengersInformation=$_SESSION['passengersInformation'];
-                    foreach ($oneWayFlight as $value) { ?>
-                    <div class="resultBox">
-                        <?php echo $value ?>
-                    </div>
-                    <?php
-                    }
-                    ?>
-                </div>
-                
-                <div class="flexResults">
-                    <?php
-                    foreach ($returnWayFlight as $value) { ?>
-                    <div class="resultBox">
-                        <?php echo $value ?>
-                    </div>
-                    <?php
-                    }
-                    ?>
-                </div>
+                <?php
+                //storing $_SESSION arrays about the flights for better use of it
+                $oneWayFlight=$_SESSION['oneWayFlight'];
+                $returnWayFlight=$_SESSION['returnFlight'];
+                $passengersInformation=$_SESSION['passengersInformation'];
+                // end of storage
+                ?>
 
+                <!-- OneWay Flight--------------------------- -->
+                <h2 class="display-8">VOL ALLER</h2>
                 <div class="flexResults">
 
                     <div class="resultBox">
-                        <?php                     
-                        echo $passengersInformation['email1'];
-                        echo $passengersInformation['email1'];
-                        echo $passengersInformation['email1'];
-                        echo $passengersInformation['email1'];
-                        echo $passengersInformation['email1'];
+                        <?php
+                        echo $oneWayFlight['flightNumber'];
                         ?>
                     </div>
+
+                    <div class="resultBox">
+                        <?php
+                        echo $oneWayFlight['departure_airport_id'] . "  " . "✈" . "  " . $oneWayFlight['arrival_airport_id'];
+                        ?>
+                    </div>
+
+                    <div class="resultBox">
+                        <?php
+                        echo $oneWayFlight['departure_time'] . "  " . "✈" . "  " . $oneWayFlight['arrival_time'];
+                        ?>
+                    </div>
+
+                    <div class="resultBox">
+                        <?php
+                        switch (array_key_last($oneWayFlight)) {
+                            case 'economy1':
+                                echo 'SAVER';
+                                break;
+                            case 'economy2':
+                                echo 'FLEX';
+                                break;
+                            case 'economy3':
+                                echo 'PREMIUM';
+                                break;
+                            case 'price_business':
+                                echo 'BUSINESS';
+                                break;
+                            };
+                        ?>
+                    </div>
+
+                    <div class="resultBox">
+                        <?php
+                        echo $_SESSION['nbPassengers'];
+                        ?>
+                    </div>
+
+                    <div class="resultBox">
+                        <?php
+                        echo $_SESSION['nbPassengers']*end($oneWayFlight) . " €";
+                        ?>
+                    </div>
+
                 </div>
+                <!-- End of OneWay Flight--------------------------------------------------- -->
+
+                <!-- Return Flight--------------------------- -->
+                <h2 class="display-8">VOL RETOUR</h2>
+                <div class="flexResults">
+
+                    <div class="resultBox">
+                        <?php
+                        echo $returnWayFlight['flightNumber2'];
+                        ?>
+                    </div>
+
+                    <div class="resultBox">
+                        <?php
+                        echo $returnWayFlight['departure_airport_id2'] . "  " . "✈" . "  " . $returnWayFlight['arrival_airport_id2'];
+                        ?>
+                    </div>
+
+                    <div class="resultBox">
+                        <?php
+                        echo $returnWayFlight['departure_time2'] . "  " . "✈" . "  " . $returnWayFlight['arrival_time2'];
+                        ?>
+                    </div>
+
+                    <div class="resultBox">
+                        <?php
+                        switch (array_key_last($returnWayFlight)) {
+                            case 'economy1_2':
+                                echo 'SAVER';
+                                break;
+                            case 'economy2_2':
+                                echo 'FLEX';
+                                break;
+                            case 'economy3_2':
+                                echo 'PREMIUM';
+                                break;
+                            case 'price_business2':
+                                echo 'BUSINESS';
+                                break;
+                            };
+                        ?>
+                    </div>
+
+                    <div class="resultBox">
+                        <?php
+                        echo $_SESSION['nbPassengers'];
+                        ?>
+                    </div>
+
+                    <div class="resultBox">
+                        <?php
+                        echo $_SESSION['nbPassengers']*end($returnWayFlight) . " €";
+                        ?>
+                    </div>
+
+                </div>
+                <!-- End of Return Flight--------------------------------------------------- -->
+
+                <!-- Owner of the order -->
+                <h2 class="display-8">Passager principal</h2>
+                <div class="flexResults">
+
+                    <div class="resultBox">
+                        <?php
+                        echo $_SESSION['firstname'];
+                        ?>
+                    </div>
+
+                    <div class="resultBox">
+                        <?php
+                        echo $_SESSION['lastname'];
+                        ?>
+                    </div>
+
+                    <div class="resultBox">
+                        <?php
+                        echo $_SESSION['birthdate'];
+                        ?>
+                    </div>
+
+                    <div class="resultBox">
+                        <?php
+                        echo $_SESSION['email'];
+                        ?>
+                    </div>
+
+                    <div class="resultBox">
+                        <?php
+                        echo $_SESSION['phone'];
+                        ?>
+                    </div>
+
+                </div>
+                <!-- End of Passenger owner--------- -->
+
+                <h2 class="display-8">
+                    Prix total:
+                    <?php
+                    echo(end($returnWayFlight)+end($returnWayFlight))*$_SESSION['nbPassengers'] . " €";
+                    ?>
+                </h2>
+                <a href="#"><button>Confirmer</button></a>
 
             </div>
         </div>
