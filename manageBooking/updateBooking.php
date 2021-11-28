007268b5036eecd1628fda22a3303998830f2b5e
simoncm <mollardsimon11@gmail.com>
2021-11-29 00:58:10 +0100

    cleaning header and footer on modification page

--- manageBooking/updateBooking.php
+++ manageBooking/updateBooking.php
@@ -15,63 +15,70 @@
 </head>
 
 <body>
-    <?php
-    @require_once "../headerLogin.php";
-    $bookingToEditId=$_GET['id'];
-    $query="SELECT booking_number,
-    oneway_flight.flight_number AS flight_number1,
-    DATE_FORMAT(oneway_flight.departure_time, '%d/%m/%Y %H:%i') AS departure_time1,
-    DATE_FORMAT(oneway_flight.arrival_time, '%d/%m/%Y %H:%i') AS arrival_time1,
-    oneway_flight.departure_airport_id AS departure_airport_id1,
-    oneway_flight.arrival_airport_id AS arrival_airport_id1,
-    return_flight.flight_number AS flight_number2,
-    DATE_FORMAT(return_flight.departure_time, '%d/%m/%Y %H:%i') AS departure_time2,
-    DATE_FORMAT(return_flight.arrival_time, '%d/%m/%Y %H:%i') AS arrival_time2,
-    return_flight.departure_airport_id AS departure_airport_id2,
-    return_flight.arrival_airport_id AS arrival_airport_id2
-    FROM user_flight 
-    JOIN flight AS oneway_flight ON flight_id=oneway_flight.id
-    LEFT JOIN flight AS return_flight ON returnflight_id=return_flight.id
-    WHERE booking_number=:bookingToEditId";
-    //preparation PDO
-    $statement = $pdo->prepare($query);
-    $statement->bindValue(':bookingToEditId', $bookingToEditId, \PDO::PARAM_STR);
-    $statement->execute();
-    //end of preparation
-    $booking=$statement->fetchAll();
-    $booking=$booking[0];
-    ?>
-    
-    <form method="post" action="updatedSuccessfully.php">
+
+
         <?php
-        foreach($booking as $key => $value) {
-            if (('departure_time1'==$key
-            || 'departure_time2'==$key
-            || 'arrival_time1'==$key 
-            || 'arrival_time2'==$key)
-            && $value != null) {
-                $formattedDate=strtotime($value);
-                $formattedDate=date('Y-m-d',$formattedDate);
-                ?>
-                <label class=""></label>
-                <input type="date" name="<?php echo $key ?>" value="<?php echo $formattedDate ?>">
-            <?php 
-            } else if ($value!=$booking['departure_time1'] 
-            && $value!=$booking['departure_time2']
-            && $value!=$booking['arrival_time1']
-            && $value!=$booking['arrival_time2']
-            && is_int($key)) {
-                echo $value;
-                echo ' ';
-            }
-        }
+        @require_once "../headerLogin.php";
+        $bookingToEditId=$_GET['id'];
+        $query="SELECT booking_number,
+        oneway_flight.flight_number AS flight_number1,
+        DATE_FORMAT(oneway_flight.departure_time, '%d/%m/%Y %H:%i') AS departure_time1,
+        DATE_FORMAT(oneway_flight.arrival_time, '%d/%m/%Y %H:%i') AS arrival_time1,
+        oneway_flight.departure_airport_id AS departure_airport_id1,
+        oneway_flight.arrival_airport_id AS arrival_airport_id1,
+        return_flight.flight_number AS flight_number2,
+        DATE_FORMAT(return_flight.departure_time, '%d/%m/%Y %H:%i') AS departure_time2,
+        DATE_FORMAT(return_flight.arrival_time, '%d/%m/%Y %H:%i') AS arrival_time2,
+        return_flight.departure_airport_id AS departure_airport_id2,
+        return_flight.arrival_airport_id AS arrival_airport_id2
+        FROM user_flight 
+        JOIN flight AS oneway_flight ON flight_id=oneway_flight.id
+        LEFT JOIN flight AS return_flight ON returnflight_id=return_flight.id
+        WHERE booking_number=:bookingToEditId";
+        //preparation PDO
+        $statement = $pdo->prepare($query);
+        $statement->bindValue(':bookingToEditId', $bookingToEditId, \PDO::PARAM_STR);
+        $statement->execute();
+        //end of preparation
+        $booking=$statement->fetchAll();
+        $booking=$booking[0];
         ?>
-        <input type="submit" value="Submit">
-    </form>
-    
-    <?php
-    require_once "../footer.html";
-    ?>
+        
+        <main class="backgroundLogin">
+
+            <form method="post" action="updatedSuccessfully.php">
+                <?php
+                foreach($booking as $key => $value) {
+                    if (('departure_time1'==$key
+                    || 'departure_time2'==$key
+                    || 'arrival_time1'==$key 
+                    || 'arrival_time2'==$key)
+                    && $value != null) {
+                        $formattedDate=strtotime($value);
+                        $formattedDate=date('Y-m-d',$formattedDate);
+                        ?>
+                        <label class=""></label>
+                        <input type="date" name="<?php echo $key ?>" value="<?php echo $formattedDate ?>">
+                    <?php 
+                    } else if ($value!=$booking['departure_time1'] 
+                    && $value!=$booking['departure_time2']
+                    && $value!=$booking['arrival_time1']
+                    && $value!=$booking['arrival_time2']
+                    && is_int($key)) {
+                        echo $value;
+                        echo ' ';
+                    }
+                }
+                ?>
+                <input class="validatebtn" type="submit" value="Confirmer la modification">
+            </form>
+            
+
+        </main>
+
+                    <?php
+            require_once "../footer.html";
+            ?>
 </body>
 
 </html>
